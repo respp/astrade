d4d86404a66a395932d3c99aae588ae51c793f43
Renzo Barcos <renzoespinosa.b@gmail.com>
2025-10-15 19:17:27 -0300

    fix: restore babel-plugin-module-resolver configuration
    - Re-add babel-plugin-module-resolver to babel.config.js
    - Configure alias mapping for @ imports
    - Restore original babel configuration that was working
    - Fix module resolution issues after rebase

--- dapp/babel.config.js
+++ dapp/babel.config.js
@@ -3,14 +3,12 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'babel-plugin-module-resolver',
       [
         'babel-plugin-module-resolver',
         {
           root: ['./'],
           alias: {
             '@': './',
-            '@dojoengine/torii-wasm': '@dojoengine/torii-wasm/pkg/web',
           },
         },
       ],
