bd308e243ac846b738bd859cd9aad97902c18412
Renzo Barcos <renzoespinosa.b@gmail.com>
2025-10-15 19:18:20 -0300

    fix: simplify metro configuration
    - Remove problematic serializer configuration that caused bundling errors
    - Keep only essential WASM file support
    - Restore minimal metro config to avoid path resolution issues
    - Fix Metro bundler crash on web platform

--- dapp/metro.config.js
+++ dapp/metro.config.js
@@ -5,16 +5,4 @@ const config = getDefaultConfig(__dirname);
 // Add support for .wasm files
 config.resolver.assetExts.push('wasm');
 
-// Add support for .wasm files in source extensions
-config.resolver.sourceExts.push('wasm');
-
-// Configure WebAssembly support
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
-// Add resolver for @dojoengine packages
-config.resolver.alias = {
-  ...config.resolver.alias,
-  '@dojoengine/torii-wasm': '@dojoengine/torii-wasm/pkg/web',
-};
-
 module.exports = config;
